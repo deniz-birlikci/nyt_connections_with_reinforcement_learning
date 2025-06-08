65d378f597a16488f56e19c7e7287c22305e1903
Mehmet Deniz Birlikci <mdbirlikci@gmail.com>
2025-06-08 16:32:15 -0700

    nice

--- verifiers/examples/nyt_connections.py
+++ verifiers/examples/nyt_connections.py
@@ -3,10 +3,10 @@ from verifiers.envs.nyt_connections_env import NYTConnectionsEnv
 
 """
 Inference:
-CUDA_VISIBLE_DEVICES=0,1,2,3 vf-vllm --model willcb/Qwen2.5-7B-NYTConnections-SFT --tensor-parallel-size 4
+CUDA_VISIBLE_DEVICES=0,1 vf-vllm --model willcb/Qwen2.5-7B-NYTConnections-SFT --tensor-parallel-size 1 --data-parallel-size 2
 
 Training:
-CUDA_VISIBLE_DEVICES=4,5,6,7 accelerate launch --config-file configs/zero3.yaml --num-processes 4 verifiers/examples/nyt_connections.py
+CUDA_VISIBLE_DEVICES=2,3,4,5,6,7 accelerate launch --config-file configs/zero3.yaml --num-processes  verifiers/examples/nyt_connections.py
 """
 
 size = '7B'
@@ -23,16 +23,13 @@ vf_env = NYTConnectionsEnv(
 run_name = f"nyt-connections-grpo-{size}"
 training_args = vf.grpo_defaults(run_name=run_name)
 training_args.num_iterations = 3
-training_args.per_device_train_batch_size = 8
+training_args.per_device_train_batch_size = 4
 training_args.num_generations = 8
-training_args.gradient_accumulation_steps = 4
+training_args.gradient_accumulation_steps = 1
 training_args.max_prompt_length = 1000
 training_args.max_completion_length = 4096
 training_args.max_steps = 10
 training_args.mask_env_responses = True
-training_args.use_vllm = True
-training_args.vllm_mode = "colocate"
-training_args.vllm_gpu_memory_utilization = 0.3
 
 trainer = vf.GRPOTrainer(
     model=model,
